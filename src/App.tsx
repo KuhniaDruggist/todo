f5340aef022198755780500f396e5bbd4df3b3b9
HP <druggist5059@gmail.com>
2021-07-20 00:39:49 +0300

    Refactor component App. Add component 'Todolist'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import Todolist from "./components/Todolist";
+
+const taskListOne = [
+    {id: 1, title: "HTML&CSS", isDone: false},
+    {id: 2, title: "JS", isDone: false},
+    {id: 3, title: "React", isDone: true}
+];
+
+const taskListTwo = [
+    {id: 1, title: "Batman", isDone: true},
+    {id: 2, title: "Agent 007", isDone: false},
+    {id: 3, title: "Terminator", isDone: true}
+];
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={"What to learn"} tasks={taskListOne}/>
+            <Todolist title={"Films"} tasks={taskListTwo}/>
         </div>
     );
 }
