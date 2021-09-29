f9027b64f747970b937a32b86203dbe0c41d13c6
KuhniaDruggist <druggist5059@gmail.com>
2021-09-29 15:31:43 +0300

    Updates

---
+++ precache-manifest.76262e22fcf254e331a749bc8ac2f5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaae9579701b58644b72e3a68a457a1b",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "80f2879707191c1ae99f",
+    "url": "/trello/static/css/main.477874f6.chunk.css"
+  },
+  {
+    "revision": "39d3bc9208efaa3d82c6",
+    "url": "/trello/static/js/2.d4a27271.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/trello/static/js/2.d4a27271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f2879707191c1ae99f",
+    "url": "/trello/static/js/main.f71460b1.chunk.js"
+  },
+  {
+    "revision": "85634081ced083bda063",
+    "url": "/trello/static/js/runtime-main.acac8ba6.js"
+  }
+]);
